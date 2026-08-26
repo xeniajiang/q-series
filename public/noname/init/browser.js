1b88cc92716653030c031cd3b77da0d92aa5b371
Xenia Jiang <xxa.xenia@gmail.com>
2026-08-26 17:32:12 +0800

    fix: support pure static browser hosting

--- public/noname/init/browser.js
+++ public/noname/init/browser.js
@@ -1,191 +1,56 @@
 import { __vitePreload } from "../../_virtual/preload-helper.js";
+
 async function browserReady({ lib, game }) {
-  lib.path = (await __vitePreload(async () => {
-    const { default: __vite_default__ } = await import("../../vendor/pnpm/path-browserify-esm@1.0.6/modules/path-browserify-esm/index.esm.js");
-    return { default: __vite_default__ };
-  }, true ? [] : void 0, import.meta.url)).default;
-  try {
-    await fetch(`/checkFile?fileName=noname.js`).then((response) => response.json()).then((result) => {
-      if (!result?.success) throw new Error(result.errorMsg);
-    });
-  } catch (e) {
-    console.error("文件读写函数初始化失败:", e);
-    return;
-  }
-  game.export = function(data, name) {
-    if (typeof data === "string") {
-      data = new Blob([data], { type: "text/plain" });
-    }
-    let fileNameToSaveAs = name || "noname";
-    fileNameToSaveAs = fileNameToSaveAs.replace(/\\|\/|:|\?|"|\*|<|>|\|/g, "-");
-    const downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    downloadLink.href = window.URL.createObjectURL(data);
-    downloadLink.click();
-  };
-  game.exit = function() {
-    window.onbeforeunload = null;
-    window.close();
-  };
-  game.open = function(url) {
-    window.open(url);
-  };
-  game.checkFile = function checkFile(fileName, callback, onerror) {
-    fetch(`/checkFile?fileName=${fileName}`).then((response) => response.json()).then((result) => {
-      if (result) {
-        if (result.success) {
-          switch (result.data) {
-            case "file":
-              callback?.(1);
-              return;
-            case "directory":
-              callback?.(0);
-              return;
-            default:
-              callback?.(-1);
-              return;
-          }
-        }
-      }
-      onerror?.(result?.errorMsg);
-    }).catch(onerror);
-  };
-  game.checkDir = function checkDir(dir, callback, onerror) {
-    fetch(`/checkDir?dir=${dir}`).then((response) => response.json()).then((result) => {
-      if (result) {
-        if (result.success) {
-          switch (result.data) {
-            case "file":
-              callback?.(0);
-              return;
-            case "directory":
-              callback?.(1);
-              return;
-            default:
-              callback?.(-1);
-              return;
-          }
-        }
-      }
-      onerror?.(result?.errorMsg);
-    }).catch(onerror);
-  };
-  game.readFile = function readFile(fileName, callback = () => {
-  }, error = () => {
-  }) {
-    fetch(`/readFile?fileName=${fileName}`).then((response) => response.json()).then((result) => {
-      if (result?.success) {
-        const data = result.data;
-        let buffer;
-        if (typeof data == "string") {
-          buffer = Uint8Array.fromBase64(data);
-        } else if (Array.isArray(data)) {
-          buffer = new Uint8Array(data);
-        }
-        callback(buffer.buffer);
-      } else {
-        error(result?.errorMsg);
-      }
-    }).catch(error);
-  };
-  game.readFileAsText = function readFileAsText(fileName, callback = () => {
-  }, error = () => {
-  }) {
-    fetch(`/readFileAsText?fileName=${fileName}`).then((response) => response.json()).then((result) => {
-      if (result?.success) {
-        callback(result.data);
-      } else {
-        error(result?.errorMsg);
-      }
-    }).catch(error);
-  };
-  game.writeFile = function writeFile(data, path, name, callback = () => {
-  }) {
-    game.ensureDirectory(path, () => {
-      if (Object.prototype.toString.call(data) == "[object File]") {
-        const fileReader = new FileReader();
-        fileReader.onload = (event) => {
-          game.writeFile(event.target.result, path, name, callback);
-        };
-        fileReader.readAsArrayBuffer(data, "UTF-8");
-      } else {
-        let filePath = path;
-        if (path.endsWith("/")) {
-          filePath += name;
-        } else if (path == "") {
-          filePath += name;
-        } else {
-          filePath += "/" + name;
-        }
-        fetch(`/writeFile`, {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            data: typeof data == "string" ? data : Array.prototype.slice.call(new Uint8Array(data)),
-            path: filePath
-          })
-        }).then((response) => response.json()).then((result) => {
-          if (result?.success) {
-            callback();
-          } else {
-            callback(result?.errorMsg);
-          }
-        });
-      }
-    });
-  };
-  game.removeFile = function removeFile(fileName, callback = () => {
-  }, error = () => {
-  }) {
-    fetch(`/removeFile?fileName=${fileName}`).then((response) => response.json()).then((result) => {
-      callback(result.errorMsg);
-    }).catch(error);
-  };
-  game.getFileList = function getFileList(dir, callback = () => {
-  }, onerror) {
-    fetch(`/getFileList?dir=${dir}`).then((response) => response.json()).then((result) => {
-      if (!result) {
-        throw new Error("Cannot get available resource.");
-      }
-      if (result.success) {
-        callback(result.data.folders, result.data.files);
-      } else if (onerror) {
-        onerror(new Error(result.errorMsg));
-      }
-    });
-  };
-  game.ensureDirectory = function ensureDirectory(list, callback = () => {
-  }, file = false) {
-    let pathArray = typeof list == "string" ? list.split("/") : list;
-    if (file) {
-      pathArray = pathArray.slice(0, -1);
-    }
-    game.createDir(pathArray.join("/"), callback, console.error);
-  };
-  game.createDir = function createDir(directory, successCallback = () => {
-  }, errorCallback = () => {
-  }) {
-    fetch(`/createDir?dir=${directory}`).then((response) => response.json()).then((result) => {
-      if (result?.success) {
-        successCallback();
-      } else {
-        errorCallback(new Error("创建文件夹失败"));
-      }
-    }).catch(errorCallback);
-  };
-  game.removeDir = function removeDir(directory, successCallback = () => {
-  }, errorCallback = () => {
-  }) {
-    fetch(`/removeDir?dir=${directory}`).then((response) => response.json()).then((result) => {
-      if (result?.success) {
-        successCallback();
-      } else {
-        errorCallback(new Error("创建文件夹失败"));
-      }
-    }).catch(errorCallback);
-  };
+	lib.path = (await __vitePreload(async () => {
+		const { default: path } = await import("../../vendor/pnpm/path-browserify-esm@1.0.6/modules/path-browserify-esm/index.esm.js");
+		return { default: path };
+	}, [], import.meta.url)).default;
+
+	const resolveAsset = fileName => new URL(String(fileName).replace(/^\/+/, ""), new URL(lib.assetURL || "./", location.href));
+	const fetchAsset = async fileName => {
+		const url = resolveAsset(fileName);
+		const response = await fetch(url);
+		const contentType = response.headers.get("content-type") || "";
+		if (!response.ok || (contentType.includes("text/html") && !url.pathname.endsWith(".html"))) {
+			throw new Error(`静态文件不存在：${fileName}`);
+		}
+		return response;
+	};
+
+	game.export = function(data, name) {
+		if (typeof data === "string") data = new Blob([data], { type: "text/plain" });
+		const link = document.createElement("a");
+		link.download = (name || "noname").replace(/\\|\/|:|\?|"|\*|<|>|\|/g, "-");
+		link.href = URL.createObjectURL(data);
+		link.click();
+		setTimeout(() => URL.revokeObjectURL(link.href), 0);
+	};
+	game.exit = function() { window.onbeforeunload = null; window.close(); };
+	game.open = function(url) { window.open(url); };
+	game.checkFile = function(fileName, callback) {
+		fetchAsset(fileName).then(() => callback?.(1)).catch(() => callback?.(-1));
+	};
+	game.checkDir = function(_dir, callback) { callback?.(-1); };
+	game.readFile = function(fileName, callback = () => {}, error = () => {}) {
+		fetchAsset(fileName).then(response => response.arrayBuffer()).then(callback).catch(error);
+	};
+	game.readFileAsText = function(fileName, callback = () => {}, error = () => {}) {
+		fetchAsset(fileName).then(response => response.text()).then(callback).catch(error);
+	};
+	game.writeFile = function(_data, _path, _name, callback = () => {}) {
+		callback(new Error("静态网页版不支持写入服务器文件"));
+	};
+	game.removeFile = function(_fileName, callback = () => {}) {
+		callback(new Error("静态网页版不支持删除服务器文件"));
+	};
+	game.getFileList = function(_dir, _callback = () => {}, onerror = () => {}) {
+		onerror(new Error("静态网页版不支持枚举服务器目录"));
+	};
+	game.ensureDirectory = function(_list, callback = () => {}) { callback(); };
+	game.createDir = function(_directory, successCallback = () => {}) { successCallback(); };
+	game.removeDir = function(_directory, _successCallback = () => {}, errorCallback = () => {}) {
+		errorCallback(new Error("静态网页版不支持删除服务器目录"));
+	};
 }
-export {
-  browserReady as default
-};
+
+export { browserReady as default };
---
+++ public/preload.js
@@ -0,0 +1,1 @@
+export { default } from "./noname/init/browser.js";
